f61cfa1bfb1a7217517e3b53be2fd943682a180c
Faith <faithmncube48@gmail.com>
2022-03-20 20:24:24 -0700

    header file

---
+++ sorting_algorithms/sort.h
@@ -0,0 +1,34 @@
+#define SORT_H
+
+#include <stddef.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in teh node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void swap_list(listint_t **list, listint_t **prev, listint_t *next);
+void swap(int *int1, int *int2);
+void quick_sort(int *array, size_t size);
+int partition(int *array, size_t size, int low, int high);
+void swap_elements(int **int1, int **int2);
+void sort(int *array, size_t size, int left, int right);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+
+#endif /* SORT_H */
